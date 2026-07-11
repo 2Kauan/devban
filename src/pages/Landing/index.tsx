e661d5c8b3c5a3ef98b1d4e3d06d8361008c33a1
Kauan <batistakauan598@gmail.com>
2026-07-11 11:11:01 -0300

    fix: remove testimonials section from landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -3,7 +3,6 @@ import { Hero } from './components/Hero';
 import { Features } from './components/Features';
 import { HowItWorks } from './components/HowItWorks';
 import { Pricing } from './components/Pricing';
-import { Testimonials } from './components/Testimonials';
 import { FAQ } from './components/FAQ';
 import { CTASection } from './components/CTASection';
 import { Footer } from './components/Footer';
@@ -16,7 +15,6 @@ export default function Landing() {
       <Features />
       <HowItWorks />
       <Pricing />
-      <Testimonials />
       <FAQ />
       <CTASection />
       <Footer />
